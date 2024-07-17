88f16f315c853d1ee2b7e214cbc3ce68eee86624
TusharIndia <kt326481@gmail.com>
2024-07-18 01:43:49 +0530

    ada

--- server/index.js
+++ server/index.js
@@ -1,24 +1,22 @@
-const express = require('express')
-const connectToMongo = require('./db')
-const cors = require('cors')
+const express = require('express');
+const connectToMongo = require('./db');
+const cors = require('cors');
 require('dotenv').config();
 
 connectToMongo();
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors(
-    {
-        origin: ["https://fit-track-iota.vercel.app/"],
-        methods: ["POST","GET", 'PUT', 'DELETE'],
-        credentials: true
-    }
-))
+app.use(cors({
+    origin: ["https://fit-track-iota.vercel.app"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true
+}));
 
-app.use('/api/user',require('./routes/user'));
-app.use('/api/workout',require('./routes/workout'));
+app.use('/api/user', require('./routes/user'));
+app.use('/api/workout', require('./routes/workout'));
 
-app.listen(PORT,()=>{
-    console.log('Listening at the port 5000')
-})
+app.listen(PORT, () => {
+    console.log(`Listening at port ${PORT}`);
+});
